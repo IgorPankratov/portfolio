fd7124d757e893db08b8e822b1841f464b506671
IgorP <ipankrat85@gmail.com>
2025-06-14 22:00:17 +0300

    Fourth screen - Skills. Complete

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,12 @@ hamburger.addEventListener('click', function () {
 close.addEventListener('click', function () {
         menu.classList.remove('active');
     });
+
+// Делаем rating-line 
+
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
